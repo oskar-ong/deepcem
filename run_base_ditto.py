fbfb6d2f5b2ebdb5c8e5caf4ab727b774abddceb
Oskar Ong <oskar.ong@studium.uni-hamburg.de>
2025-12-06 17:21:22 +0100

    testing run base

--- run_base_ditto.py
+++ run_base_ditto.py
@@ -82,12 +82,12 @@ if __name__=="__main__":
             "--batch_size", "32",
             "--max_len", "128",
             "--lr", "3e-5",
-            "--n_epochs", "10",
+            "--n_epochs", "1",
             "--finetuning",
             "--lm", "roberta",
             "--fp16",
             "--save_model",
-            "--logdir", checkpoint_dir,
+            "--logdir", "./models/ditto/checkpoints/",
         ]
 
         env = os.environ.copy()
