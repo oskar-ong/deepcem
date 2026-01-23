84a0aab7d68b28b552d343295f41a0134f96e7d1
Oskar Ong <oskar.ong@studium.uni-hamburg.de>
2026-01-23 12:59:46 +0100

    add experiment sbatch

---
+++ experiment.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Request one Task (unless using mpi4py)
+#SBATCH --job-name=deepcem-experiment
+#SBATCH --partition=epyc-gpu
+#SBATCH --time=1-0
+
+# Request memory per CPU
+#SBATCH --mem-per-cpu=32G
+# Request n CPUs for your task.
+#SBATCH --cpus-per-task=1
+# Request GPU Ressources (model:number)
+#SBATCH --gpus=a100:1
+
+# Clear all interactively loaded modules
+module purge
+
+# Load a python package manager
+module load anaconda # or micromamba or anaconda
+
+# Activate a certain environment
+conda activate deepcem2
+ 
+# set number of OpenMP threads (i.e. for numpy, etc...)
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+# if you are adding your own level of parallelzation, you
+# probably want to set OMP_NUM_THREADS=1 instead, in order 
+# to prevent the creation of too many threads (massive slowdown!)
+
+# export cache
+export HF_HOME=/hpc/gpfs2/scratch/u/zeru47vu/hf
+export HF_HUB_CACHE=/hpc/gpfs2/scratch/u/zeru47vu/hf/hub
+
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+
+# No need to pass number of tasks to srun
+#srun python run_latefusion.py
+srun python experiment.py --config 
